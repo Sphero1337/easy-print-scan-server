8a5db6a97ff3e262675c48bad06c0412f46e2fdf
Dominik Wagner <dominik.wagner@trench-group.com>
2024-11-22 09:57:12 +0100

    added loop for restarting

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from app.main import PrinterScannerApp
 
 if __name__ == "__main__":
-    app = PrinterScannerApp()
-    app.run()
+    while True:
+        try:
+            app = PrinterScannerApp()
+            app.run()
+        except Exception as e:
+            print("application crashed, will restart\n%s" % e)
